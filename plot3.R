18092b24834d16c3f4777219d51601c031c15117
pwvirgo <pwvirgo@gmail.com>
2015-01-22 22:31:21 -0500

    1st 5 plots completed

--- plot3.R
+++ plot3.R
@@ -1,9 +1,28 @@
-## Coursera Exploratory Data Analysis  Project 2 Plot 2
-
+## Coursera Exploratory Data Analysis  Project 2 Plot 3
+#-----------------------------------------------------------------------------
+# Of the four types of sources indicated by the type (point, nonpoint, onroad,
+#	nonroad) variable, which of these four sources have seen decreases in
+#   emissions from 1999–2008 for Baltimore City?
+#   Which have seen increases in emissions from 1999–2008?
+#   Use the ggplot2 plotting system to make a plot answer this question.
+#-----------------------------------------------------------------------------
 setwd("~/a/highEd/dataScience_coursera/ExploreData/data")
 NEI<-readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
 
 ns<-NEI[NEI$fips=="24510",]   # city of Balitmore
 
+ns$type<-as.factor(ns$type)
+# add up the total emssions by type and year
+pdata<-aggregate(ns$Emissions,list(ns$year,ns$type),FUN="sum")
 
+library(ggplot2)
+g<-ggplot(pdata, aes(Group.1, x))
+
+g+geom_point(size=4) + facet_grid(Group.2~.) + geom_smooth(method=lm) +
+	labs(title="Yearly total emissions increased for type POINT") +
+	labs(x="Year") + labs(y=expression("log " * PM[2.5]))
+
+dev.copy(png, "Plot3.png", height=480, width=480)
+dev.off(4)
+				   
---
+++ plot5.R
@@ -0,0 +1,41 @@
+## Coursera Exploratory Data Analysis  Project 2 Plot 5
+#-----------------------------------------------------------------------------
+# How have emissions from motor vehicle sources changed from 1999–2008
+#  in Baltimore City?
+#-----------------------------------------------------------------------------
+setwd("~/a/highEd/dataScience_coursera/ExploreData/data")
+NEI<-readRDS("exdata-data-NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
+
+
+nei<-NEI[NEI$fips=="24510",c("SCC","year")]
+
+wanted<-function(text) {  grepl("Highway Veh", text, ignore.case="T") }
+
+# create a locical vector of wanted SCC rows 
+gotya<-sapply(SCC$Short.Name,wanted)
+
+# create a character vector of SCC codes for the Coal Comb
+scc<-as.character(SCC[gotya, "SCC"])
+
+# finally select the rows and cols for the plot
+nei=NEI[(NEI$SCC %in% scc) & NEI$fips=="24510", c("Emissions", "year")]
+
+pd<-aggregate(nei$Emissions,list(nei$year),FUN=sum)
+
+library(ggplot2)
+qplot(as.factor(pd$Group.1),pd$x, geom="bar", stat="identity",
+	  main=paste("Total highway vehicles emissions in Baltimore MD",
+	  		   "\ndeclined from 1999-2008"),
+	  ylab=expression("Total tons of " * PM[2.5]), xlab="Year") +
+	theme(title=element_text(size=rel(1.25)),
+		  axis.text.x=element_text(size=rel(2)))
+
+dev.copy(png, "Plot5.png", height=480, width=480)
+dev.off(4)
+
+
+
+
+
+
